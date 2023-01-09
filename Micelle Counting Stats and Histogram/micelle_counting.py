eda652f7ee2b3de615c7c7ae2fd5e3fbd7cedbfa
Steve <steven.t.g.street@gmail.com>
2023-01-09 13:56:47 -0800

    custom binning and x axis labels is now working!

--- Micelle Counting Stats and Histogram/micelle_counting.py
+++ Micelle Counting Stats and Histogram/micelle_counting.py
@@ -57,21 +57,31 @@ def getnormhistogram(datatoplot):
         #determine what bin sizes, scales and ticks to use
         if round(max(datatoplot)) < 20: 
                 binsize = 2
-                xaxis_max = 30
+                binrange = 0,2,4,6,8,10,12,14,16,18,20
+                xaxis_max = 22
         elif round(max(datatoplot)) < 100:
                 binsize = 10
-                xaxis_max = 150
+                binrange = 0,10,20,30,40,50,60,70,80,90,100
+                xaxis_max = 110
         elif round(max(datatoplot)) < 200:
                 binsize = 20
-                xaxis_max = 300
+                binrange = 0,20,40,60,80,100,120,140,160,180,200
+                xaxis_max = 220
         elif round(max(datatoplot)) < 500:
                 binsize = 50
-                xaxis_max = 600
+                binrange = 0,50,100,150,200,250,300,350,400,450,500
+                xaxis_max = 550
         elif round(max(datatoplot)) < 1000:
                 binsize = 100
+                binrange = 0,100,200,300,400,500,600,700,800,900,1000
                 xaxis_max = 1100
+        elif round(max(datatoplot)) < 2000:
+                binsize = 200
+                binrange = 0,200,400,600,800,1000,1200,1400,1600,1800,2000
+                xaxis_max = 2200
         else:
-                binsize = 100
+                binsize = 200
+                binrange = 10
                 xaxis_max = round(max(datatoplot)+100)
         
         #plot the data as a histogram too
@@ -79,14 +89,14 @@ def getnormhistogram(datatoplot):
         plt.figure(1, figsize=(2.5,2.5), dpi= 600)
 
         plt.hist(datatoplot,
-                bins=binsize,
+                bins=binrange,
                 density=True,
                 stacked=True,
                 color="r",
                 edgecolor="k",
         #          label=dir_name
                 )
-        plt.xticks(np.arange(0, xaxis_max), binsize))
+        plt.xticks(np.arange(0, xaxis_max, binsize*2))
         plt.yticks(np.arange(0, 0.08, 0.01))
         plt.xlabel("Length (nm)")
         plt.ylabel("Normalized Count")
